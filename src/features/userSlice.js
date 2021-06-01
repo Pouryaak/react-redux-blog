7ab78fcb448a64b53faf2e91b4aab838a946128e
Pourya Akrami <pourya@Pouryas-MacBook-Pro.local>
2021-06-01 15:46:23 +0800

    changed title

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,19 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  isSignedIn: false,
+  useData: null,
+  searchInput: "tech",
+  blogData: null,
+};
+
 const userSlice = createSlice({
   name: "user",
-  initialState: {
-    isSignedIn: false,
-    userData: null,
-    searchInput: "tech",
-    blogData: null,
-  },
+  initialState,
   reducers: {
     setSignedIn: (state, action) => {
       state.isSignedIn = action.payload;
     },
     setUserData: (state, action) => {
-      state.userData = action.payload;
+      state.useData = action.payload;
     },
     setInput: (state, action) => {
       state.searchInput = action.payload;
@@ -26,7 +28,6 @@ const userSlice = createSlice({
 
 export const { setSignedIn, setUserData, setInput, setBlogData } =
   userSlice.actions;
-
 export const selectSignedIn = (state) => state.user.isSignedIn;
 export const selectUserData = (state) => state.user.userData;
 export const selectUserInput = (state) => state.user.searchInput;
